e5e630c3201b38172a2de19f59ce5fce7059e7e2
Michael Veroux <michael@veroux.com>
2025-01-29 08:16:55 +0100

    Factorisation

--- src/Game/Pack.php
+++ src/Game/Pack.php
@@ -6,64 +6,43 @@ use Iterator;
 
 class Pack implements Iterator
 {
-    /**
-     * @var Card[]
-     */
-    protected $cards;
-
-    /**
-     * @var Card[]
-     */
-    protected $pile;
+    protected $cards = [];
+    protected $pile = [];
 
     public function __construct()
     {
         $this->init();
     }
 
-    /**
-     * @return void
-     */
     protected function init()
     {
-        $tenCards = [-1, 1, 2, 3, 4, 5, 6, 7 ,8, 9, 10, 11, 12];
-        $fiveCards = [-2];
-        $fifteenCards = [0];
-
-        foreach ($tenCards as $tenCard) {
-            for ($i = 0; $i < 10; $i++) {
-                $card = new Card($tenCard);
-                $this->cards[] = $card;
-            }
-        }
-        foreach ($fiveCards as $fiveCard) {
-            for ($i = 0; $i < 5; $i++) {
-                $card = new Card($fiveCard);
-                $this->cards[] = $card;
-            }
-        }
-        foreach ($fifteenCards as $fifteenCard) {
-            for ($i = 0; $i < 15; $i++) {
-                $card = new Card($fifteenCard);
-                $this->cards[] = $card;
-            }
-        }
+        $this->cards = array_merge(
+            $this->createCards([-1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 10),
+            $this->createCards([-2], 5),
+            $this->createCards([0], 15)
+        );
 
         shuffle($this->cards);
     }
 
-    protected function invisibilizeCards()
+    private function createCards(array $values, int $count)
     {
-        $invisibilize = function (Card $card) {
-            $card->setState(Card::PACK_INVISIBLE);
-        };
-
-        array_map($invisibilize, $this->cards);
+        $cards = [];
+        foreach ($values as $value) {
+            for ($i = 0; $i < $count; $i++) {
+                $cards[] = new Card($value);
+            }
+        }
+        return $cards;
     }
 
-    /**
-     * @return false|Card
-     */
+    protected function invisibilizeCards()
+    {
+        array_map(function (Card $card) {
+            $card->setState(Card::PACK_INVISIBLE);
+        }, $this->cards);
+    }
+
     public function unstack()
     {
         $current = $this->current();
@@ -84,65 +63,39 @@ class Pack implements Iterator
         return $card;
     }
 
-    /**
-     * @param Card $card
-     * @return $this
-     */
     public function pileAdd(Card $card)
     {
         $this->pile[] = $card->setState(Card::PACK_VISIBLE);
-        
         return $this;
     }
 
-    /**
-     * @return false|Card
-     */
     public function pileVisible()
     {
         return end($this->pile);
     }
 
-    /**
-     * @return false|Card
-     */
     public function current()
     {
         return current($this->cards);
     }
 
-    /**
-     * @return void
-     */
     public function next()
     {
         next($this->cards);
     }
 
-    /**
-     * @return int|string|null
-     */
     public function key()
     {
-        $key = key($this->cards);
-
-        return $key;
+        return key($this->cards);
     }
 
-    /**
-     * @return bool
-     */
     public function valid()
     {
         return (bool)current($this->cards);
     }
 
-    /**
-     * @return void
-     */
     public function rewind()
     {
         reset($this->cards);
     }
-
 }
--- src/Game/Player.php
+++ src/Game/Player.php
@@ -4,107 +4,65 @@ namespace Mv\Skyjo\Game;
 
 class Player
 {
-    /**
-     * @var string
-     */
     protected $name;
-    /**
-     * @var Card[]
-     */
-    protected $cards;
-
-    /**
-     * @var PackProxy
-     */
+    protected $cards = [];
     protected $packProxy;
 
-    /**
-     * @param string $name
-     * @param PackProxy $packProxy
-     */
     public function __construct($name, PackProxy $packProxy)
     {
         $this->name = $name;
         $this->packProxy = $packProxy;
     }
 
-    /**
-     * @return $this
-     */
     protected function colEqualsManage()
     {
-        $i = 0;
-        while (array_key_exists($i, $this->cards) && $i < 4) {
-            $card = $this->cards[$i];
-            if (null !== $card && $card->isEqual($this->cards[$i + 4]) && $card->isEqual($this->cards[$i + 8])) {
-                $this->packProxy->pileAdd($card);
+        for ($i = 0; $i < 4; $i++) {
+            if (isset($this->cards[$i], $this->cards[$i + 4], $this->cards[$i + 8]) &&
+                $this->cards[$i]->isEqual($this->cards[$i + 4]) &&
+                $this->cards[$i]->isEqual($this->cards[$i + 8])) {
+                $this->packProxy->pileAdd($this->cards[$i]);
                 $this->cards[$i] = null;
                 $this->packProxy->pileAdd($this->cards[$i + 4]);
                 $this->cards[$i + 4] = null;
                 $this->packProxy->pileAdd($this->cards[$i + 8]);
                 $this->cards[$i + 8] = null;
             }
-
-            ++$i;
         }
-
         return $this;
     }
 
-    /**
-     * @param Card $card
-     * @return $this
-     */
     public function addCard(Card $card)
     {
         $this->cards[] = $card;
-
         return $this;
     }
 
-    /**
-     * @return void
-     */
     public function revealAll()
     {
-        $visibilize = function (Card $card = null) {
-            if ($card)
+        array_map(function (Card $card = null) {
+            if ($card) {
                 $card->setState(Card::PLAYER_VISIBLE);
-        };
-
-        array_map($visibilize, $this->cards);
+            }
+        }, $this->cards);
         $this->colEqualsManage();
     }
 
-    /**
-     * @return int
-     */
     public function getVisibleScore()
     {
-        $score = 0;
-
-        foreach ($this->cards as $card) {
-            if (null !== $card && Card::PLAYER_VISIBLE === $card->getState()) {
-                $score += $card->getValue();
-            }
-        }
-
-        return $score;
+        return array_reduce($this->cards, function ($score, Card $card = null) {
+            return $score + ($card && $card->getState() === Card::PLAYER_VISIBLE ? $card->getValue() : 0);
+        }, 0);
     }
 
     public function __toString()
     {
         $display = PHP_EOL;
-        $j = 0;
-        while (array_key_exists($j, $this->cards)) {
-            for ($i = 0; $i < 4; $i++) {
-                $val = $this->cards[$j];
-                $display .= sprintf("%s\t", null !== $val?$val:"NN");
-                $j+= 1;
+        for ($i = 0; $i < count($this->cards); $i += 4) {
+            for ($j = 0; $j < 4; $j++) {
+                $display .= isset($this->cards[$i + $j]) ? $this->cards[$i + $j] . "\t" : "NN\t";
             }
-            $display.= PHP_EOL;
+            $display .= PHP_EOL;
         }
-
         return $display;
     }
 }
